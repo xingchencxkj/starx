5957eb0021987df33281c3d38f29b70c107a5576
am-abudu <Linqi070905>
2020-03-22 14:42:36 +0800

    'upload'

--- 上传.bat
+++ 上传.bat
@@ -8,5 +8,5 @@ if not exist %GenFile% (
 git add .
 git commit -m 'upload'
 git remote rm origin
-git remote add origin https://e.coding.net/xingchencxkj/starx/xingchenjuhesousuo.git
+git remote add origin https://github.com/xingchencxkj/starx.git
 git push origin master
